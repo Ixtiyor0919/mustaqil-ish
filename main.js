49489813b93701b638afb6f992c3d5b01440d919
Ixtiyor0919 <Komiljonov2002@outlook.com>
2022-04-29 15:27:27 +0500

    o'zgarishlar kiritildi

--- main.js
+++ main.js
@@ -8,9 +8,9 @@
 // elheader.textContent = userInput;
 alert('Web sayt xizmatiga xush kelibsiz!');
 
-var userName = prompt('Ismingizni kiriting:', 'Ixtiyor');
-var secondName = prompt('Familyangizni kiriting:', 'Komiljonov');
-var studyName = prompt(`Qayerda o'qisiz?`);
+var userName = prompt('Ismingizni kiriting:', 'John');
+var secondName = prompt('Familyangizni kiriting:', 'Doll');
+var studyName = prompt(`Xurmatli mijoz qayerda o'qisiz?`);
 //DOM elemnts
 var elHeader = document.querySelector('.heading');
 //User inputs
@@ -19,6 +19,8 @@ var userInput = prompt(`${userName} Qaysi sohasida o'qisiz`)
 elHeader.textContent = userInput;
 
 var studyName = prompt(`${studyName}Front-end kursidami?:`);
+
+var studyName = prompt(`${studyName}E'tibor uchun rahmat!`, `sog' bo'lin`);
 
 console.log(firstName + ''+ secondName);
 console.log(elHeader)
